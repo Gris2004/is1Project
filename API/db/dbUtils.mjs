ca36fd131f84fc282803892ca58dfd3080820ced
Gris <gris@griscyan.gris.com>
2026-01-21 02:34:34 -0600

    feat(API/db/project.db): add two tables: products and users

--- API/db/dbUtils.mjs
+++ API/db/dbUtils.mjs
@@ -13,5 +13,5 @@ dotenv.config({path: envPath});
  *dbPath takes the value from the .env and create an absolute path for open the db
  *@return [path.resolve] dbRoute - the variable for open the database from any directory
 */
-export const dbPath = path.resolve(__dirname, "./SalesPoint.db");
+export const dbPath = path.resolve(__dirname, "./project.db");
 export const db = new sqlite3.Database(dbPath);
